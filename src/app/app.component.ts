55ed35d48e8c28675d33e77946a38253d9b5a41c
isabelmaito <maitoisabel@gmail.com>
2024-12-17 21:03:53 -0300

    Add css input e normalize e remover acentos de pesquisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,20 +35,26 @@ export class AppComponent {
 
   filtroPorTexto: string = '';
 
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
   filtrarContatosPorTexto(): Contato[] {
     if (!this.filtroPorTexto) {
       return this.contatos;
     }
     return this.contatos.filter((contato) => {
-      return contato.nome
+      return this.removerAcentos(contato.nome)
         .toLowerCase()
-        .includes(this.filtroPorTexto.toLowerCase());
+        .includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
     });
   }
 
   filtrarContatosPorLetraInicial(letra: string): Contato[] {
     return this.filtrarContatosPorTexto().filter((contato) => {
-      return contato.nome.toLowerCase().startsWith(letra);
+      return this.removerAcentos(contato.nome)
+        .toLowerCase()
+        .startsWith(this.removerAcentos(letra).toLowerCase());
     });
   }
 }
